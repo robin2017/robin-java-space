840ac6eee3ac017c87d3109cd100842beb1970b2
luobin3 <luobin3@360.cn>
2022-11-30 14:10:36 +0800

    first commit

---
+++ utils/src/main/java/com/robin/space/App.java
@@ -0,0 +1,15 @@
+package com.robin.space;
+import static com.robin.space.utils.Print.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        print("hello print");
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ utils/src/main/java/com/robin/space/utils/Print.java
@@ -0,0 +1,10 @@
+package com.robin.space.utils;
+
+public class Print {
+    public static void print(Object obj){
+        System.out.println(obj);
+    }
+    public static void printnb(Object obj){
+        System.out.print(obj);
+    }
+}
